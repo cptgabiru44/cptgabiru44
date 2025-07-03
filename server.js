0642c3e9c911f52f37bcc078b316b99b16d32591
cptgabiru44 <promotionshopee@gmail.com>
2025-07-03 20:23:05 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: '*' }));
 
 // Seu token da Pushin Pay
 const PUSHINPAY_TOKEN = '36539|loe6HF1qlPYAQtM3nRCO7ApFbH2y3PmNk4G2ld9La4aa7117';
